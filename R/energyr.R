bdfa2631809641a85920086dd913b68a31b8d120
paulgovan <pgovan1@aggienetwork.com>
2016-06-29 20:14:43 -0500

    R package

---
+++ R/energyr.R
@@ -0,0 +1,16 @@
+#' An R package containing Federal Energy Regularity Commission (FERC) data
+#'
+#' energyr is an R package containing data from http://www.ferc.gov.
+#'
+#' @section energyr functions:
+#' electric
+#' gas
+#' hydropower
+#' lng
+#' oil
+#' pipeline
+#' storage
+#' 
+#' @docType package
+#' @name energyr
+NULL
